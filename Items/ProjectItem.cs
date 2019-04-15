1490104ddb510cc6f48862ebfc72968b3e5f75e7
Adam Johnson <adamj537@gmail.com>
2019-04-15 09:26:14 -0500

    Consolidated files that contain example settings.

--- Items/ProjectItem.cs
+++ Items/ProjectItem.cs
@@ -1,12 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace GenericTreeView
 {
 	[Serializable]
+	public class MaterialItem : NamedObjectItem
+	{
+		public MaterialItem(string name) : base(name)
+		{
+
+		}
+	}
+
+	[Serializable]
+	public class ImageBackgroundItem : NamedObjectItem
+	{
+		public ImageBackgroundItem(string name) : base(name)
+		{
+
+		}
+
+		public Image Image { get; set; }
+	}
+
+	[Serializable]
+	public class GradientBackgroundItem : BackgroundItem
+	{
+		public GradientBackgroundItem(string sName, Color color1, Color color2) : base(sName)
+		{
+			Color1 = color1;
+			Color2 = color2;
+		}
+
+		[TreeNode]
+		public Color Color1 { get; set; }
+
+		[TreeNode]
+		public Color Color2 { get; set; }
+	}
+
+	[Serializable]
+	public class ComponentItem : NamedObjectItem
+	{
+		public ComponentItem() : base("Component")
+		{
+
+		}
+
+		public ComponentItem(string name) : base(name)
+		{
+
+		}
+	}
+
+	[Serializable]
+	public class AssemblyItem : NamedObjectItem
+	{
+		public AssemblyItem() : this("Assembly")
+		{
+
+		}
+
+		public AssemblyItem(string sName) : base(sName)
+		{
+			Components = new ContainerItem<ComponentItem>("Components");
+		}
+
+		[TreeNode]
+		public ContainerItem<ComponentItem> Components { get; set; }
+	}
+
+	[Serializable]
+	public class BackgroundItem : NamedObjectItem
+	{
+		public BackgroundItem(string name) : base(name)
+		{
+
+		}
+	}
+
+	/// <summary>
+	/// ContainerItem is a list and also implements INamedObjectItem, so the container can have a name
+	/// This will save creating specific classes for each container
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	[Serializable]
+	public class ContainerItem<T> : List<T>, INamedObjectItem
+	{
+		public ContainerItem(string name)
+		{
+			Name = name;
+		}
+
+		[TreeNode]
+		public string Name { get; set; }
+	}
+
+	[Serializable]
 	public class ProjectItem : NamedObjectItem
 	{
-		public ProjectItem(string sName)
-		  : base(sName)
+		public ProjectItem(string name) : base(name)
 		{
 			Assemblies = new ContainerItem<AssemblyItem>("Assemblies");
 			Backgrounds = new ContainerItem<BackgroundItem>("Backgrounds");
